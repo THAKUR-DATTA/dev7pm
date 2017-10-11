274ef0b78b9269d2e8e8368c56c2081eb816797a
THAKUR-DATTA <dthakur001@gmail.com>
2017-10-11 18:15:46 +0000

    this is the demo3.c programm in that c lang code is their

---
+++ demo3.c
@@ -0,0 +1,26 @@
+Example 1: Program to print half pyramid using *
+*
+* *
+* * *
+* * * *
+* * * * *
+
+include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+    {
+        for(j=1; j<=i; ++j)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
